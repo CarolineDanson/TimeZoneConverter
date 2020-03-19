7ade4a20ada5ddc6c9bdd1d716a95ddcd2ba4936
Caroline Danson <carro.danson@gmail.com>
2020-03-19 22:28:45 +0100

    Tried to get the javascript to work(drf spagetti) and changed som html

--- timezonejs.js
+++ timezonejs.js
@@ -15,9 +15,35 @@ let utc = utc + (3600000 * offset);
 let nd = new Date();
 document.writeln("Bombay time is " + nd.toLocaleString() + "<br>");*/
 
+let city1 = document.getElementById("city1").value;
+let utc1 = document.getElementById("city1").value;
+
+let city2 = document.getElementById("city2").value;
+let utc2 = document.getElementById("utc2").value;
+
+let result;
+
+function calcTimeZones() {
+
+    // create Date object for current location
+    let d = new Date();
+
+    // convert to msec
+    // add local time zone offset
+    // get UTC time in msec
+    utc1 = d.getTime() + (d.getTimezoneOffset() * 60000);
+    utc2 = d.getTime() + (d.getTimezoneOffset() * 60000);
+    let td = (utc1 / 3600000) - (utc2 / 3600000);
+    // create new Date object for different city
+    // using supplied offset
+    //let nd = new Date((utc1 + (3600000 * offset) - (utc2 + (3600000 * offset)));
+
+    // return time as a string
+    return document.getElementById("result").innerText = "The time difference between" + city1 + " & " + city2 + " is " +(td).toFixed(2);
+}
 
 
-function calcTimeZones(city, offset) {
+/*function calcTimeZones(city, offset) {
 
         // create Date object for current location
         let d = new Date();
@@ -32,6 +58,6 @@ function calcTimeZones(city, offset) {
         let nd = new Date(utc + (3600000 * offset));
 
         // return time as a string
-        return "The local time in " + city + " is " + nd.toLocaleString()
-}
+        return document.getElementById("result").innerText = "The local time in " + city + " is " + nd.toLocaleString();
+}*/
 
