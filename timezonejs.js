2315a1cdf5c5b397578ff77936088bc74af3d6ac
Caroline Danson <carro.danson@gmail.com>
2020-03-20 15:03:48 +0100

    *

--- timezonejs.js
+++ timezonejs.js
@@ -1,20 +1,3 @@
-/*//Objekt med nuvarande tid
-let date = new Date();
-//Lokaltid sedan Jan.1 1970
-let localTime = date.getTime();
-//Tidszon konverterat till millisekunder
-let localOffset = date.getTimezoneOffset() * 60000;
-//Få nuvarande UTC tid.
-let utc = localTime + localOffset;
-
-//UTC variabeln
-let offset;
-//Lägger till destinationens tidzon till utc i millisekunder.
-let utc = utc + (3600000 * offset);
-//Konverterar UTC i millisec till något läsbart
-let nd = new Date();
-document.writeln("Bombay time is " + nd.toLocaleString() + "<br>");*/
-
 let city1 = document.getElementById("timezone").value;
 let utc1 = document.getElementById("city1").value;
 
@@ -23,60 +6,114 @@ let utc2 = document.getElementById("utc2").value;
 
 let result;
 
+let mt;
 
-/*function GetSelectedText(){
-    var e = document.getElementById("timezone1");
-    var result = e.options[e.selectedIndex].text;
-    
-    document.getElementById("result").innerHTML = result;
-}*/
+// convert to msec since Jan 1 1970
+localTime = d.getTime();
 
-function GetSelectedValue(){
-    var e = document.getElementById("timezone1");
+// obtain local UTC offset and convert to msec
+localOffset = d.getTimezoneOffset() * 60000;
+document.getElementById("mt").innerHTML = "hej";
+
+
+
+function GetSelectedValue() {
+    //var e = document.getElementById("timezone1");
     var b = document.getElementById("timezone2");
-    var result2 = e.options[e.selectedIndex].value;
-    var result3 = b.options[e.selectedIndex].value;
-    var sum = result2 + result3;
-    
-    document.getElementById("result").innerHTML = result2;
+    //var result2 = e.options[e.selectedIndex].value;
+    var result3 = b.options[b.selectedIndex].value;
+    //var sum = result2 + result3;
+
+
+
+    /*var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+
+    var lokaltid = new Date();
+    lokaltid.setHours(lokaltid.getHours());
+
+    var d2 = lokaltid.setHours(lokaltid.getHours() - Date.parse(result3));
+    //d2.setHours(d2.getHours() - result3);*/
+
+    // create Date object for current location
+    d = new Date();
+    //Express this local time as the number of milliseconds since Jan. 1, 1970, by invoking the Date()object's getTime() method:
+
+    // convert to msec since Jan 1 1970
+    localTime = d.getTime();
+
+    // obtain local UTC offset and convert to msec
+    localOffset = d.getTimezoneOffset() * 60000;
+
+    // obtain UTC time in msec
+    utc = localTime + localOffset;
+
+    // obtain and add destination's UTC time offset
+    // for example, Bombay
+    // which is UTC + 5.5 hours
+    //offset = result3;
+    bombay = utc + (3600000 * (result3));
+
+    // convert msec value to date string
+    nd = new Date(bombay);
+    //document.writeln("Bombay time is " + nd.toLocaleString() + "<br>");
+
+    // create Date object for current location
+    d = new Date();
+   
+    // convert to msec
+    // add local time zone offset
+    // get UTC time in msec
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+   
+    // create new Date object for different city
+    // using supplied offset
+    nd = new Date(utc + (3600000*(result3)));
+   
+    // return time as a string
+    //return "The local time in " + city + " is " + nd.toLocaleString();
+
+    document.getElementById("result").innerHTML = nd.toLocaleString();
+
+    //document.getElementById("result").innerHTML = result2;
 }
 
 
 function calcTimeZones() {
 
-   /* // create Date object for current location
+    /*// create Date object for current location
+     let d = new Date();
+ 
+     // convert to msec
+     // add local time zone offset
+     // get UTC time in msec
+     utc1 = d.getTime() + (d.getTimezoneOffset() * 60000);
+     utc2 = d.getTime() + (d.getTimezoneOffset() * 60000);
+     let td = (utc1 / 3600000) - (utc2 / 3600000);
+     // create new Date object for different city
+     // using supplied offset
+     let nd = new Date((utc1 + (3600000 * offset) - (utc2 + (3600000 * offset))));
+ 
+     // return time as a string
+     return document.getElementById("result").innerText = "The time difference between" + city1 + " & " + city2 + " is " +(td).toFixed(2);*/
+}
+
+
+function calcTimeZones(city, offset) {
+
+    /*// create Date object for current location
     let d = new Date();
 
     // convert to msec
     // add local time zone offset
     // get UTC time in msec
-    utc1 = d.getTime() + (d.getTimezoneOffset() * 60000);
-    utc2 = d.getTime() + (d.getTimezoneOffset() * 60000);
-    let td = (utc1 / 3600000) - (utc2 / 3600000);
+    let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
     // create new Date object for different city
     // using supplied offset
-    //let nd = new Date((utc1 + (3600000 * offset) - (utc2 + (3600000 * offset)));
+    let nd = new Date(utc + (3600000 * offset));
 
     // return time as a string
-    return document.getElementById("result").innerText = "The time difference between" + city1 + " & " + city2 + " is " +(td).toFixed(2);*/
+    return document.getElementById("result").innerText = "The local time in " + city + " is " + nd.toLocaleString();*/
 }
-
-
-/*function calcTimeZones(city, offset) {
-
-        // create Date object for current location
-        let d = new Date();
-
-        // convert to msec
-        // add local time zone offset
-        // get UTC time in msec
-        let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-
-        // create new Date object for different city
-        // using supplied offset
-        let nd = new Date(utc + (3600000 * offset));
-
-        // return time as a string
-        return document.getElementById("result").innerText = "The local time in " + city + " is " + nd.toLocaleString();
-}*/
 
